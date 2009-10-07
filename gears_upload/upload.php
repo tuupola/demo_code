14f25220f7d2aec09286e74091511ee5aeddd9f9
Mika Tuupola <tuupola@appelsiini.net>
2009-10-07 18:19:52 +0300

    Support for uploading multiple images by drag and dropping.

--- gears_upload/upload.php
+++ gears_upload/upload.php
@@ -7,21 +7,26 @@ $error_message[3] = "File was only partially uploaded.";
 $error_message[4] = "Choose a file to upload.";
 
 $upload_dir  = './tmp/';
-$upload_file = $upload_dir . basename($_FILES['user_file']['name']);
+$num_files = count($_FILES['user_file']['name']);
 
-if (!preg_match("/(gif|jpg|jpeg|png)$/",$_FILES['user_file']['name'])) {
-    print "I asked for an image...";
-} else {
-    if (is_uploaded_file($_FILES['user_file']['tmp_name'])) {
-        if (move_uploaded_file($_FILES['user_file']['tmp_name'], $upload_file)) {
-            /* Success... */
+for ($i=0; $i < $num_files; $i++) {
+    $upload_file = $upload_dir . basename($_FILES['user_file']['name'][$i]);
+
+    if (!preg_match("/(gif|jpg|jpeg|png)$/",$_FILES['user_file']['name'][$i])) {
+        print "I asked for an image...";
+    } else {
+        if (is_uploaded_file($_FILES['user_file']['tmp_name'][$i])) {
+            if (move_uploaded_file($_FILES['user_file']['tmp_name'][$i], $upload_file)) {
+                /* Success... */
+            } else {
+                print $error_message[$_FILES['user_file']['error']];
+            }     
         } else {
             print $error_message[$_FILES['user_file']['error']];
-        }     
-    } else {
-        print $error_message[$_FILES['user_file']['error']];
+        }    
     }    
 }
+
 
 
 
